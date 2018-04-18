1712a1dc678133eb0acd8cb551042e2a524fd40a
DinossoLate <ignalelop@alum.us.es>
2018-04-18 17:32:03 +0200

    ADD Comented the program's logic

--- front-end/fronEnd/App.js
+++ front-end/fronEnd/App.js
@@ -37,45 +37,58 @@ const instructions = Platform.select({
 
 type Props = {};
 
+// Definimos la tarea a ejecutar
 BackgroundTask.define(async () => {
+  // En este caso, se trata de intentar obtener las notificaciones del servidor
   fetch("http://192.168.1.37:3001/test", {
       method: "GET"
     }).then((response) => response.json())
-      .then((responseData) => {
+	.then((responseData) => {
+	// Aqui se realizaría el tratamiento de notificaciones
         console.warn("Envía notificación");
         PushNotification.localNotification({
           title: responseData.notification.title,
-          message: responseData.notification.description,
-          actions: responseData.notification.actions
+          message: responseData.notification.description, // Este atributo es necesario para que se muestre la notificación, no obviarlo!
+          actions: responseData.notification.actions // Con este atributo, podemos definir diferentes opciones que se mostrarán
         });
+	// Avisamos de la finalización de la tarea
         BackgroundTask.finish();
       }).catch((err) => {
         console.err(err);
+	// En el caso de que no se pueda obtener la notificación, volvemos a avisar  
         BackgroundTask.finish();
       });
-  BackgroundTask.schedule();
+    // Finalmente, para asegurar la tarea repetida, volvemos a solicitar una tarea en 2º plano
+    BackgroundTask.schedule({
+      period: 40 // Un periodo de 40 equivale a menos de 15 minutos, pero esto no es considerado por el SO salvo en casos especificos (aseguramos de intentar pedirlo antes de 15 minutos
+    );
 })
 
 export default class App extends Component<Props> {
+  // Definimos la configuración del sistema de notificaciones
   configureNotifications = () => {
     PushNotification.configure({
       onRegister: (token) => {
-        console.log("Registered with token " + token);
+        console.log("Registered with token " + token); // El token (creo) que reconoce al equipo, pero no nos haría falta en este caso
       },
 
       onNotification: (notification) => {
+        // Cuando haya una notificación. ¿Debemos realizar algo? Aqui se pondrá 
         console.log("Notification received " + notification);
 
+	// Confirmamos la realización de la notificación
         notification.finish();
       }
     })
   }
 
+  // Con esto podremos configurar el handler de las acciones definidas, debería estar fuera de la actividad!
   configureNotificationHandler = () => {
     PushNotification.registerNotificationActions(['I\'m making a note here', 'Huge success!']);
     DeviceEventEmitter.addListener("notificationActionReceived", (action) => {
       console.log("Received action " + action);
-      const info = JSON.parse(action.dataJSON);
+			const info = JSON.parse(action.dataJSON);
+			// Con este `if`, detectaremos si la acción es de un tipo u otro
       if (info.action == 'Huge success!') {
         alert("It appears so");
       }
@@ -83,6 +96,7 @@ export default class App extends Component<Props> {
 
   }
 
+	// Ejemplo de creación de notificación
   sendTestingNotification = () => {
     PushNotification.localNotification({
       id: 0,
@@ -96,7 +110,8 @@ export default class App extends Component<Props> {
   }
 
   constructor() {
-    super();
+		super();
+		// Indicamos el handler, de nuevo esto se definiría afuera del componente
     this.configureNotificationHandler();
   }
 
@@ -106,6 +121,7 @@ export default class App extends Component<Props> {
     );
   }
 
+	// Este es importante, para asegurar que la tarea en 2º plano se ejecuta, debemos llamar aqui a `.schedule()`
   componentDidMount() {
     BackgroundTask.schedule({
       period: 400
